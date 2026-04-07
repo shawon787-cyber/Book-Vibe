13f6276ed7ca6149c54e053357bdd7f317b447a3
Md Shawon <shawonmohammad787@gmail.com>
2026-04-08 00:26:25 +0600

    books layout

---
+++ src/Components/UI/BookCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { CiStar } from "react-icons/ci";
+const BookCard = ({book}) => {
+    return (
+        <div>
+             <div className="card bg-gray-100  shadow-sm">
+              <figure className='p-6'>
+                <img className='h-[350px] w-full rounded-2xl'
+                  src={book.image}
+                  alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <div className='flex gap-2'>
+                    {
+                    book.tags.map((tag, ind) => (
+                        <div key={ind} className="badge text-green-500 bg-green-100 font-bold">{tag}</div>
+                    ))
+                }
+                </div>
+                  <h2 className='font-bold text-2xl'>{book.bookName}</h2>
+                <p className='font-semibold text-lg'>{book.author}</p>
+                <div className="card-actions justify-between border-t border-dashed pt-4">
+                  <div className="badge badge-outline">{book.category}</div>
+                  <div className="badge badge-outline">{book.rating} <CiStar /></div>
+                </div>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
--- src/Pages/Homepage/AllBooks.jsx
+++ src/Pages/Homepage/AllBooks.jsx
@@ -1,6 +1,7 @@
 
 import { use } from 'react';
-import { CiStar } from "react-icons/ci";
+
+import BookCard from '../../Components/UI/BookCard';
 
 const bookPromise = fetch('/booksData.json')
 .then(res=> res.json())
@@ -11,32 +12,11 @@ const AllBooks = () => {
     return (
         <div className='my-12 max-w-7xl mx-auto'>
             <h2 className='font-bold text-3xl text-center'>Books</h2>
-            <div className='grid lg:grid-cols-3 gap-6 lg:gap-8 justify-center lg:justify-between'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 justify-center lg:justify-between px-2 mt-6'>
                 {
-                books.map(book => {
+                books.map((book, ind) => {
                     return (
-                        <div className="card bg-gray-100  shadow-sm">
-  <figure className='p-6'>
-    <img className='h-[350px] w-full rounded-2xl'
-      src={book.image}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <div className='flex gap-2'>
-        {
-        book.tags.map((tag) => (
-            <div className="badge text-green-500 bg-green-100 font-bold">{tag}</div>
-        ))
-    }
-    </div>
-      <h2 className='font-bold text-2xl'>{book.bookName}</h2>
-    <p className='font-semibold text-lg'>{book.author}</p>
-    <div className="card-actions justify-between border-t border-dashed pt-4">
-      <div className="badge badge-outline">{book.category}</div>
-      <div className="badge badge-outline">{book.rating} <CiStar /></div>
-    </div>
-  </div>
-</div>
+                        <BookCard key={ind} book={book}></BookCard>
                     )
                 })
             }
